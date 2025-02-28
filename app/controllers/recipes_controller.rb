7a317c528211f571923c480f305f296254bdaf05
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2025-02-27 22:06:34 -0300

    add recipes views and controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,48 @@
 class RecipesController < ApplicationController
-  def index
-  end
+    before_action :set_recipe, only: %i[ show edit update destroy ]
+
+    def index
+      @recipes = Recipe.all
+    end
+
+    def show
+    end
+
+    def new
+      @recipe = Recipe.new
+    end
+
+    def create
+      @recipe = Recipe.new(recipe_params)
+      if @recipe.save
+        redirect_to @recipe
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if @recipe.update(recipe_params)
+        redirect_to @recipe
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @recipe.destroy
+      redirect_to recipes_path
+    end
+
+    private
+      def set_recipe
+        @recipe = Recipe.find(params[:id])
+      end
+
+      def recipe_params
+        params.expect(recipe: [ :name, :description, :time, :portions, :rating, :ingredients, :instruction ])
+      end
 end
