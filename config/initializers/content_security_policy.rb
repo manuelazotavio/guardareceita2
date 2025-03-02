75529bce79ceba4c91d33c824ab32200d864137e
Manuela Otavio <o.manuela@aluno.ifsp.edu.br>
2025-03-02 08:38:56 -0300

    Update content_security_policy.rb

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,9 +1,20 @@
-Rails.application.config.middleware.insert_before 0, "Rack::Cors", debug: true,
-logger: (-> { Rails.logger }) do
-  allow do
-    origins "*"
-    resource "*",
-        headers: :any,
-        methods: [ :get, :post, :delete, :put, :patch, :options, :head ]
-    end
-end
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src :self, :https, "http://localhost:3000"
+    policy.font_src    :self, :https, :data
+    policy.connect_src :self, :https, "http://localhost:3000"
+    policy.img_src     :self, :https, :data
+    policy.object_src  :none
+    policy.script_src  :self, :https, "http://localhost:3000"
+    policy.style_src   :self, :https, "http://localhost:3000"
+    # Specify URI for violation reports
+    # policy.report_uri "/csp-violation-report-endpoint"
+  end
+
+
+  # Generate session nonces for permitted importmap, inline scripts, and inline styles.
+  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  config.content_security_policy_nonce_directives = %w[script-src style-src]
+
+  # Report violations without enforcing the policy.
+  # config.content_security_policy_report_only = true
