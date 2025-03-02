4934999b67374359c1c66b3a3887cc5fa2ee0e5e
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2025-03-02 08:29:01 -0300

    add localhost to connect_src

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,15 +1,17 @@
 
 Rails.application.configure do
   config.content_security_policy do |policy|
-    policy.default_src :self, :https, "localhost:3000"
+    policy.default_src :self, :https, "http://localhost:3000"
     policy.font_src    :self, :https, :data
+    policy.connect_src :self, :https, "http://localhost:3000"
     policy.img_src     :self, :https, :data
     policy.object_src  :none
-    policy.script_src  :self, :https
-    policy.style_src   :self, :https
+    policy.script_src  :self, :https, "http://localhost:3000"
+    policy.style_src   :self, :https, "http://localhost:3000"
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
   end
+
 
   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
