63297d9a08612f379ad981b8d4680443f9c1dfd5
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2025-03-09 12:38:55 -0300

    feat: user crud ok

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+    allow_unauthenticated_access
+    skip_forgery_protection
+      before_action :set_user, only: %i[ show edit update destroy ]
+
+      def index
+        @users = User.all
+      end
+
+      def show
+      end
+
+      def new
+        @user = User.new
+      end
+
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          puts "user post ok"
+        else
+          render :new, status: :unprocessable_entity
+        end
+      end
+
+      def edit
+      end
+
+      def update
+        if @user.update(user_params)
+          puts "user update ok"
+        else
+          render :edit, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        if @user.destroy
+          puts "user delete ok"
+        else
+          render :destroy, status: :unprocessable_entity
+        end
+      end
+
+      private
+        def set_user
+          @user = User.find(params[:id])
+        end
+
+        def user_params
+          params.expect(user: [ :email_address, :password_digest ])
+        end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   resources :recipes
+  resources :users
 end
