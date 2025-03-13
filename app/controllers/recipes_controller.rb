5dc480b9c19f7ef66d22c785375b51927a07f508
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2025-03-13 17:49:58 -0300

    feat: add favorited in update recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -27,7 +27,7 @@ class RecipesController < ApplicationController
     end
 
     def update
-      if @recipe.update(recipe_params)
+      if @recipe.update(recipe_update_params)
         puts "recipe update ok"
       else
 
@@ -59,5 +59,9 @@ class RecipesController < ApplicationController
 
       def recipe_params
         params.expect(recipe: [ :name, :description, :user_id, :time, :portions, :rating, :ingredients, :instruction ])
+      end
+
+      def recipe_update_params
+        params.expect(recipe: [ :name, :description, :user_id, :time, :portions, :rating, :ingredients, :instruction, :favorited ])
       end
 end
