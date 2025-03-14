151b1a5ec2b5cc206f30857ae8932fac9319c80e
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2025-03-14 15:32:28 -0300

    add name to table users

--- db/schema.rb
+++ db/schema.rb
@@ -89,6 +89,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_06_174126) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
